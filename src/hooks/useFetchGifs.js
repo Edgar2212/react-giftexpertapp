93ddc363f3dd2b0fa7a21a1ca278ebe191c0478c
edgar mend <edgar,mend2212881@gmail.com>
2022-01-16 01:41:51 -0400

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,47 @@
+
+
+import { useEffect, useState } from "react"
+import { getGifs } from '../components/helpers/getGifs';
+
+
+
+export const useFetchGifs = ( category ) => {
+    
+    const [state, setstate] = useState({
+        data: [],
+        loading: true
+    });
+
+        useEffect( () => {
+
+           getGifs( category )
+            .then( imgs =>{
+
+               
+                    
+                    setstate({
+                        data: imgs,
+                        loading: false
+                    });
+
+               
+
+                
+            } )
+
+        },[category])
+
+
+
+    
+
+    return state;
+
+
+}
+
+/* useEffect( () => {
+    getGifts( category ) 
+        .then( setImages ) ;     
+}, [ category ])     */
+ 
